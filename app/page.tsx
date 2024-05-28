3d4c95570fd5a11b8cc08456ab567eadc370dae6
Yasser Razzaki <yasserrazzaki0@gmail.com>
2024-05-28 16:29:15 +0200

    chapter 12 of the course

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
      
     <main className="flex min-h-screen flex-col p-6">
-     
+     <p>Dashboard Page</p>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {<AcmeLogo />}
       </div>

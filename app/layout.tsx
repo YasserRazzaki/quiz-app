036fec5466d12bd22a66cba4dbb9f08c9ae34416
Yasser Razzaki <yasserrazzaki0@gmail.com>
2024-05-29 09:07:36 +0200

    add:login.
    Fin de la course avec un projet opérationnel.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+import NavLinks from './ui/dashboard/nav-links';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
  
 export default function RootLayout({
   children,
@@ -8,7 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div className="flex">
+          <nav className="w-1/4 bg-gray-100 p-4">
+            <NavLinks />
+          </nav>
+          <main className="w-3/4 p-4">
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   );
 }

2c83a4b22e3e3fa8a338ed9cba71425df698a5f2
Yasser Razzaki <yasserrazzaki0@gmail.com>
2024-06-17 12:41:49 +0200

    login fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   return (
     <main className="flex items-center bg-gray-100 justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+        <div className="flex w-full items-end rounded-lg">
           <div className="w-32 text-white md:w-36">
           </div>
         </div>

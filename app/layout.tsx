a1177deb6a39dca399bf80873811ed00ea4e2507
Yasser Razzaki <yasserrazzaki0@gmail.com>
2024-06-17 12:32:48 +0200

    Add logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+import { signOut } from '@/auth';
 import Link from 'next/link';
-//import NavLinks from './ui/dashboard/nav-links';
  
 export const metadata: Metadata = {
   title: {
@@ -18,26 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
- /*    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <div className="flex">
-          <nav className="w-1/4 bg-gray-100 p-4">
-            <NavLinks />
-          </nav>
-          <main className="w-3/4 p-4">
-            {children}
-          </main>
-        </div>
-      </body>
-    </html> */
-    <html>
+<html>
     <body>
       <div className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
               <Link href="/" legacyBehavior>
-                <a className="text-white font-bold text-xl">Mon Site !</a>
+                <a className="text-white font-bold text-xl">QuizApp</a>
               </Link>
             </div>
             <div className="hidden md:block" >
@@ -51,9 +39,16 @@ export default function RootLayout({
                 <Link href="/dashboard/quiz-api" legacyBehavior>
                   <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Quiz BDD</a>
                 </Link>
-                <Link href="/logout" legacyBehavior>
-                  <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Log out</a>
-                </Link>
+                <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
+          <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
               </div>
             </div>
           </div>

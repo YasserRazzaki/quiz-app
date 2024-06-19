a8b9c493bb241303cab1fcc2e2847fb9e8013dd6
Yasser Razzaki <yasserrazzaki0@gmail.com>
2024-06-19 08:45:23 +0200

    fixings rankings : only rankingMap

--- app/dashboard/rankings/page.tsx
+++ app/dashboard/rankings/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { getRankingsApi, getRankingsMap } from '../../lib/data';
-import { RankingAPI, RankingMap } from '../../lib/definitions';
+import { getRankingsMap } from '../../lib/data';
+import { RankingMap } from '../../lib/definitions';
 
 const Rankings = () => {
-  const [rankingsAPI, setRankingsAPI] = useState<RankingAPI[]>([]);
   const [rankingMap, setRankingMap] = useState<RankingMap[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -14,17 +13,13 @@ const Rankings = () => {
         setLoading(true);
         console.log('Fetching rankings...');
 
-        const apiData = await getRankingsApi();
-        console.log('Rankings API data:', apiData);
-
         const mapData = await getRankingsMap();
         console.log('Ranking Map data:', mapData);
 
         // Trier les données par score décroissant
-        apiData.sort((a, b) => b.score - a.score);
         mapData.sort((a, b) => b.score - a.score);
 
-        setRankingsAPI(apiData);
+        // Mettre à jour l'état avec les nouvelles données
         setRankingMap(mapData);
       } catch (error) {
         console.error('Failed to fetch rankings:', error);
@@ -45,30 +40,6 @@ const Rankings = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4 space-y-8">
-      <div className="w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4 text-center">Rankings API</h1>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <table className="w-full bg-white border border-gray-300 mb-8">
-            <thead>
-              <tr>
-                <th className="py-2 border border-gray-300 text-center">Username</th>
-                <th className="py-2 border border-gray-300 text-center">Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              {rankingsAPI.map((ranking, index) => (
-                <tr key={index}>
-                  <td className="py-2 border border-gray-300 text-center">{ranking.username}</td>
-                  <td className="py-2 border border-gray-300 text-center">{ranking.score}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-
       <div className="w-full max-w-md">
         <h1 className="text-2xl font-bold mb-4 text-center">Ranking Map</h1>
         {loading ? (
